e3845ad4eaa7e75d5af4a4d3d1f7472c7d941e76
Emiliano <EmilianoR117@outlook.com>
2025-11-02 13:37:35 -0600

    update: changes in main

--- App-Spark/src/main/java/org/digitalnao/Main.java
+++ App-Spark/src/main/java/org/digitalnao/Main.java
@@ -1,17 +1,26 @@
 package org.digitalnao;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.digitalnao.controller.ApiController;
+import org.digitalnao.dao.UserDao;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import static spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        port(8080);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "");
+        jdbi.installPlugin(new SqlObjectPlugin());
+
+        UserDao dao = jdbi.onDemand(UserDao.class);
+        dao.createTable();
+
+
+        ApiController.initRoutes(dao);
+
+        System.out.println("🚀 Server running on http://localhost:8080");
+        System.out.println("🔌 API REST: http://localhost:8080/api/users");
+
     }
 }
