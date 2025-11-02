703ef751206f57c921f5c0e8a51135d73fd9012c
Emiliano <EmilianoR117@outlook.com>
2025-11-02 13:35:55 -0600

    feat: creation of the api controller

---
+++ App-Spark/src/main/java/org/digitalnao/controller/ApiController.java
@@ -0,0 +1,58 @@
+package org.digitalnao.controller;
+
+import org.digitalnao.dao.UserDao;
+import org.digitalnao.model.User;
+import com.google.gson.Gson;
+import static spark.Spark.*;
+
+public class ApiController {
+    public static void initRoutes(UserDao dao) {
+        Gson gson = new Gson();
+
+        get("/users", (req, res) -> {
+            res.type("application/json");
+            return gson.toJson(dao.getAll());
+        });
+
+        get("/users/:id", (req, res) -> {
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            User user = dao.findById(id);
+            if (user == null) {
+                res.status(404);
+                return gson.toJson("User not found");
+            }
+            return gson.toJson(user);
+        });
+
+        post("/users", (req, res) -> {
+            res.type("application/json");
+            User user = gson.fromJson(req.body(), User.class);
+            dao.insert(user);
+            res.status(201);
+            return gson.toJson("User created");
+
+        });
+
+        put("/users/:id", (req, res) -> {
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            User user = gson.fromJson(req.body(), User.class);
+            user.setId(id);
+            dao.update(user);
+            return gson.toJson(user);
+        });
+
+        delete("/users/:id", (req, res) -> {
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            User user = dao.findById(id);
+            if (user == null) {
+                res.status(404);
+                return gson.toJson("User not found");
+            }
+            dao.delete(id);
+            return gson.toJson("User deleted");
+        });
+    }
+}
