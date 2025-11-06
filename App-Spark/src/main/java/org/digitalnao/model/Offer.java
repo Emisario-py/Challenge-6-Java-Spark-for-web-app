9a79673dd26da943f7f903821e981a6f77ba4ca3
Emiliano <EmilianoR117@outlook.com>
2025-11-05 19:56:24 -0600

    update: add of fields

--- App-Spark/src/main/java/org/digitalnao/model/Offer.java
+++ App-Spark/src/main/java/org/digitalnao/model/Offer.java
@@ -2,10 +2,17 @@ package org.digitalnao.model;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Offer {
     private int id;
     private int userId;
     private int itemId;
+    private String itemName;
+    private String UserName;
+    private Date createAt;
+    private String formattedDate;
+    private String status;
     private double amount;
 }
